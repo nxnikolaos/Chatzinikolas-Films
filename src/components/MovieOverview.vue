961cec356f839072d6161d8e69f98e5aaa89d212
Chatzinikolas Nikos IS <nxnatlas@gmail.com>
2023-12-11 13:48:48 +0200

    MovieOverview component implemented.

--- src/components/MovieOverview.vue
+++ src/components/MovieOverview.vue
@@ -1,6 +1,42 @@
-<script setup></script>
+<script setup>
+const { post, imgUrl } = defineProps({ post: Object, imgUrl: String })
+</script>
 <template>
-  <h2>Why</h2>
+  <section class="flex p-8 items-center max-w-screen-lg">
+    <section class="p-4">
+      <img class="max-w-xs" :src="imgUrl" />
+    </section>
+    <section class="text-white p-4 [&>p]:py-2 [&>p]:text-lg [&>p>span]:font-bold">
+      <h2 class="text-2xl font-bold">{{ post.original_title }}</h2>
+      <p>
+        Release Date : <span>{{ post.release_date }}</span>
+      </p>
+      <p>
+        Genres :
+        <span
+          class="after:content-[',_'] after:last:content-['']"
+          v-for="genre in post.genres"
+          :key="genre.id"
+          >{{ genre.name }}</span
+        >
+      </p>
+      <p>{{ post.overview }}</p>
+      <p>
+        Runtime : <span>{{ post.runtime }} minutes.</span>
+      </p>
+      <div class="flex justify-between [&>p>span]:font-bold">
+        <p>
+          Popularity : <span>{{ post.popularity }}</span>
+        </p>
+        <p>
+          Rating : <span>{{ post.vote_average }}</span>
+        </p>
+        <p>
+          Vote Count : <span>{{ post.vote_count }}</span>
+        </p>
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
--- src/components/pages/MovieDetails.vue
+++ src/components/pages/MovieDetails.vue
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import MovieOverview from '../MovieOverview.vue'
 
 const route = useRoute()
 const pageId = route.params.id
@@ -23,8 +24,6 @@ onMounted(() => {
       const results = response.data
       movie.value = results
       posterUrl.value = `https://image.tmdb.org/t/p/w342` + movie.value.poster_path
-
-      console.log(movie)
     })
     .catch(function (error) {
       console.log(error)
@@ -32,41 +31,7 @@ onMounted(() => {
 })
 </script>
 <template>
-  <section class="flex p-8 items-center max-w-screen-lg">
-    <section class="p-4">
-      <img class="max-w-xs" :src="posterUrl" />
-    </section>
-    <section class="text-white p-4 [&>p]:py-2 [&>p]:text-lg [&>p>span]:font-bold">
-      <h2 class="text-2xl font-bold">{{ movie.original_title }}</h2>
-      <p>
-        Release Date : <span>{{ movie.release_date }}</span>
-      </p>
-      <p>
-        Genres :
-        <span
-          class="after:content-[',_'] after:last:content-['']"
-          v-for="genre in movie.genres"
-          :key="genre.id"
-          >{{ genre.name }}</span
-        >
-      </p>
-      <p>{{ movie.overview }}</p>
-      <p>
-        Runtime : <span>{{ movie.runtime }} minutes.</span>
-      </p>
-      <div class="flex justify-between [&>p>span]:font-bold">
-        <p>
-          Popularity : <span>{{ movie.popularity }}</span>
-        </p>
-        <p>
-          Rating : <span>{{ movie.vote_average }}</span>
-        </p>
-        <p>
-          Vote Count : <span>{{ movie.vote_count }}</span>
-        </p>
-      </div>
-    </section>
-  </section>
+  <MovieOverview :post="movie" :imgUrl="posterUrl"></MovieOverview>
 </template>
 
 <script>
